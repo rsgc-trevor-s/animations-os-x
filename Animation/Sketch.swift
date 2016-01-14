ac0640f711b0d3655c28e352e1fe133f114b25de
rsgc-trevor-s <student@MacBook-Pro-706-Trevor.local>
2016-01-14 10:03:38 -0500

    Installed Mr. Gordon’s audio kit and started to fool around with it

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,29 +9,43 @@
 import Foundation
 
 class Sketch {
-
+    
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
     // Declare any properties you need for your sketch below this comment, but before init()
     var x = 0
+    var y = 0   // Vertical position of circle, will be tied to amplitude of mic input
     var s = 1
-
+    
+    // Objects needed to read mic input and analyze it
+    var analyzer: AKAudioAnalyzer!
+    let microphone = AKMicrophone()
+    let analysisSequence = AKSequence()
+    var updateAnalysis: AKEvent?
+    
     // This runs once, equivalent to setup() in Processing
     init() {
         
         // Create canvas object – specify size
-        canvas = Canvas(width: 700, height: 100)
+        canvas = Canvas(width: 500, height: 300)
         
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
+        
+        // Initialize objects used to analyze mic input
+        analyzer = AKAudioAnalyzer(input: microphone.output)
+        AKOrchestra.addInstrument(microphone)
+        AKOrchestra.addInstrument(analyzer)
+        analyzer.play()
+        microphone.play()
         
     }
     
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
-                
+        
         // Horizontal position of circle
         x = x + s
         
@@ -40,15 +54,21 @@ class Sketch {
             s *= -1
         }
         
-        // Clear the background
+        // Print amplitude of mic input (seems to vary between 0 and 1)
+        print("Amplitude as a float is: \(analyzer.trackedAmplitude.floatValue)")
+        
+        // Tie the vertical position of the circle to the amplitude
+        y = Int( Float(canvas.height) * analyzer.trackedAmplitude.floatValue )
+        
+        // "Clear" the background with a semi-transparent black rectangle
         canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
         canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
         
         // Draw a circle that moves across the screen
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
+        canvas.drawEllipse(centreX: 30, centreY: y+20, width: 100, height: 5)
         
     }
     
