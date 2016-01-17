5ec4fa2b78158736eb90f792cf71d7d8e6e18dcc
rsgc-trevor-s <student@MacBook-Pro-706-Trevor.local>
2016-01-17 16:47:24 -0500

    Made some minor changes, and tried to connect points with lines. Almost Final product if not final.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -18,13 +18,19 @@ class Sketch {
     var rectangleHeightTrebble = 0   // Vertical position of rectangle, will be tied to amplitude of mic input
     var rectangleHeightSoprano = 0
     var rectangleHeightBass = 0
-    var ellipseSize = 0
-     var circleX = 255
+    var circleYValues = 0
+    var ellipseHeight = 0
+    var circleX = 25
     var circleY = 50
+    
+    var lastX = 0
+    var lastY = 0
+    var s = 1
     
     var height = 1280
     var width = 800
-    var gap = 125
+    
+    var gap = 140
     var hue = Float(120) //green
     var hue2 = Float(60) //yellow
     var hue3 = Float(10) //red
@@ -52,7 +58,9 @@ class Sketch {
         AKOrchestra.addInstrument(analyzer)
         analyzer.play()
         microphone.play()
-         canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: 100)
+        
+        
+        canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: 100)
     }
     
     // Runs repeatedly, equivalent to draw() in Processing
@@ -63,22 +71,37 @@ class Sketch {
         
         // Tie the height of the rectangle to the amplitude
         //Instead of having different frequencies, there is just ratios between the 3 main ranges in music
-
+        
         rectangleHeightSoprano = Int(Float(5000) * analyzer.trackedAmplitude.floatValue)
         rectangleHeightTrebble = Int(Float(10000) * analyzer.trackedAmplitude.floatValue)
         rectangleHeightBass = Int(Float(7500) * analyzer.trackedAmplitude.floatValue)
         
-        ellipseSize = Int(Float(500) * analyzer.trackedAmplitude.floatValue)
+        ellipseHeight = Int(Float(500) * analyzer.trackedAmplitude.floatValue)
+        
         
         // "Clear" the background with a semi-transparent black rectangle
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
         canvas.drawRectangle(bottomRightX: 0, bottomRightY: 100, width: canvas.width, height: canvas.height)
         
-       
-        canvas.fillColor = Color(hue: 40, saturation: 80, brightness: 90, alpha: 90)
-        canvas.drawEllipse(centreX: circleX, centreY: circleY, width: ellipseSize, height: ellipseSize)
         
+        
+        //creates a circle that moves along the screen until the end where it then earases everything and restarts
+        circleX = circleX+s
+        if circleX>canvas.width{
+            circleX=25
+            canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: 100)
+        }
+        
+        canvas.fillColor = Color(hue: 32, saturation: 100, brightness: 100, alpha: 100)
+        canvas.drawEllipse(centreX: circleX, centreY: circleY+ellipseHeight, width: 2, height: 2)
+        
+        //        lastX = circleX-s
+        //        lastY = circleY
+        
+        //        //connects the points from the old circle position to the new one with a line
+        //        canvas.fillColor = Color(hue: 80, saturation: 100, brightness: 90, alpha: 100)
+        //        canvas.drawLine(fromX: lastX, fromY: lastY, toX: circleX, toY: ellipseHeight+circleY)
         
         
         
@@ -100,7 +123,15 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(3*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //green rectangles Soprano shifted again
+        canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 90, alpha: 90)
+        for index in 0...11 {
+            if rectangleHeightSoprano >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(6*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         //green rectangles Trebble
         canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 90, alpha: 90)
         for index in 0...11 {
@@ -115,7 +146,15 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(4*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //green rectangles Trebble shifted again
+        canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 90, alpha: 90)
+        for index in 0...11 {
+            if rectangleHeightTrebble >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(7*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         //green rectangles Bass
         canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 90, alpha: 90)
         for index in 0...11 {
@@ -124,6 +163,7 @@ class Sketch {
             }
         }
         
+        
         //green rectangles Bass shifted
         canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 90, alpha: 90)
         for index in 0...11 {
@@ -131,7 +171,15 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(5*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //green rectangles Bass shifted again
+        canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 90, alpha: 90)
+        for index in 0...11 {
+            if rectangleHeightBass >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(8*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         //yellow bars Soprano
         canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
         for index2 in 12...24 {
@@ -140,6 +188,7 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25, bottomRightY: yPosition[index2], width: 100, height: 20, borderWidth: 50)
             }
         }
+        
         //yellow bars Soprano shifted
         canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
         for index2 in 12...24 {
@@ -148,7 +197,16 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(3*gap), bottomRightY: yPosition[index2], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //yellow bars Soprano shifted again
+        canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
+        for index2 in 12...24 {
+            
+            if(rectangleHeightSoprano>=yPosition[index2]){
+                canvas.drawRectangle(bottomRightX: 25+(6*gap), bottomRightY: yPosition[index2], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         //yellow bars Trebble
         canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
         for index in 12...24 {
@@ -163,7 +221,15 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(4*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //yellow bars Trebble shifted again
+        canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
+        for index in 12...24 {
+            if rectangleHeightTrebble >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(7*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         //yellow bars Bass
         canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
         for index in 12...24 {
@@ -178,8 +244,16 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(5*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
-
+        //yellow bars Bass shifted again
+        canvas.fillColor = Color(hue: hue2, saturation: 80, brightness: 90, alpha: 90)
+        for index in 12...24 {
+            if rectangleHeightBass >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(8*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
+        
         //red bars soprano
         canvas.fillColor = Color(hue: hue3, saturation: 80, brightness: 90, alpha: 90)
         for index3 in 25...28 {
@@ -194,7 +268,15 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(3*gap), bottomRightY: yPosition[index3], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //red bars soprano shifted again
+        canvas.fillColor = Color(hue: hue3, saturation: 80, brightness: 90, alpha: 90)
+        for index3 in 25...28 {
+            if(rectangleHeightSoprano>=yPosition[index3]){
+                canvas.drawRectangle(bottomRightX: 25+(6*gap), bottomRightY: yPosition[index3], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         
         //red bars Trebble
         canvas.fillColor = Color(hue: hue3, saturation: 80, brightness: 90, alpha: 90)
@@ -210,7 +292,15 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(4*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //red bars Trebble Shifted again
+        canvas.fillColor = Color(hue: hue3, saturation: 80, brightness: 90, alpha: 90)
+        for index in 25...28 {
+            if rectangleHeightTrebble >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(7*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
+        
         
         //red bars Bass
         canvas.fillColor = Color(hue: hue3, saturation: 80, brightness: 90, alpha: 90)
@@ -226,7 +316,14 @@ class Sketch {
                 canvas.drawRectangle(bottomRightX: 25+(5*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
             }
         }
-
+        //red bars Bass shifted again
+        canvas.fillColor = Color(hue: hue3, saturation: 80, brightness: 90, alpha: 90)
+        for index in 25...28 {
+            if rectangleHeightBass >= yPosition[index] {
+                canvas.drawRectangle(bottomRightX: 25+(8*gap), bottomRightY: yPosition[index], width: 100, height: 20, borderWidth: 50)
+            }
+        }
+        
         
         
         
